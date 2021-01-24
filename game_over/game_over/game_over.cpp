f30806b368bcfd507853b93e9a4a6541186c1029
Samuel Walker <sewalker191@gmail.com>
2021-01-24 13:30:08 -0800

    Finish game over program

--- game_over/game_over/game_over.cpp
+++ game_over/game_over/game_over.cpp
@@ -1,11 +1,13 @@
 // game_over.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Game Over
+// A first C++ program
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Game Over!\n";
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
