2b59179b2cd41b89c07adc035afb82c04c56ac5c
Samuel Walker <sewalker191@gmail.com>
2021-01-24 13:58:12 -0800

    add calculator program

---
+++ expensive_calculator/expensive_calculator/expensive_calculator.cpp
@@ -0,0 +1,19 @@
+// Expensive Calculator
+// Does some Math
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "7 + 3 = " << 7 + 3 << endl;
+	cout << "7 - 3 = " << 7 - 3 << endl;
+	cout << "7 * 3 = " << 7 * 3 << endl;
+	cout << "7 / 3 = " << 7 / 3 << endl;
+	cout << "7.0 / 3.0 = " << 7.0 / 3.0 << endl;
+	cout << "7 % 3 = " << 7 % 3 << endl;
+	cout << "7 + 3 * 5 = " << 7 + 3 * 5 << endl;
+	cout << "(7 + 3) * 5 = " << (7 + 3) * 5 << endl;
+	return 0;
+}
