923d7e9e72933d5289fd39e2e5afd9e643274480
Samuel Walker <sewalker191@gmail.com>
2021-01-25 08:44:55 -0800

    Add game_stats program

---
+++ game_stats/game_stats/game_stats.cpp
@@ -0,0 +1,78 @@
+// Game Stats
+// Declaring and initializing variables
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+/* int main()
+{
+	int score; // -2147483648 - 2147483647 // unsigned int 0 - 4294967295
+	// float: 3.4E +- 38
+	double distance; // 1.7E +- 308
+	char playAgain;
+	bool shieldsUp;
+
+	short lives, aliensKilled; // holds integers from -32768 - 32767; unsigned short int 0 - 65535
+
+	score = 0;
+	distance = 1200.76;
+	playAgain = 'y';
+	shieldsUp = true;
+	lives = 3;
+	aliensKilled = 10;
+	double engineTemp = 6572.89;
+
+	cout << "\nscore: " << score << endl;
+	cout << "distance: " << distance << endl;
+	cout << " playAgain: " << playAgain << endl;
+
+	cout << "lives: " << lives << endl;
+	cout << "aliensKilled: " << aliensKilled << endl;
+	cout << "engineTemp: " << engineTemp << endl;
+
+	int fuel;
+	cout << "\nHow much fuel? ";
+	cin >> fuel;
+	cout << "fuel: " << fuel << endl;
+
+	typedef unsigned short int ushort;
+	ushort bonus = 10;
+	cout << "\nbonus: " << bonus << endl;
+	return 0;
+} */
+
+int main()
+{
+	unsigned int score = 5000;
+	cout << "score: " << score << endl;
+
+	score = score + 100;
+	cout << "score: " << score << endl;
+
+	score += 100;
+	cout << "score: " << score << endl;
+
+	int lives = 3;
+	++lives;
+	cout << "lives: " << lives << endl;
+	
+	lives = 3;
+	lives++;
+	cout << "lives: " << lives << endl;
+
+	lives = 3;
+	int bonus = ++lives * 10;
+	cout << "lives, bonus: " << lives << ", " << bonus << endl;
+
+	lives = 3;
+	bonus = lives++ * 10;
+	cout << "lives, bonus: " << lives << ", " << bonus << endl;
+
+	score = 4294967295;
+	cout << "\nscore: " << score << endl;
+	++score;
+	cout << "score: " << score << endl;
+	return 0;
+}
