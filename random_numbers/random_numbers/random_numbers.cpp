c2801c055ae0c89b1575cd8d4200e8bef4dd3d89
Samuel Walker <sewalker191@gmail.com>
2021-01-27 15:46:23 -0800

    Add random numbers

---
+++ random_numbers/random_numbers/random_numbers.cpp
@@ -0,0 +1,21 @@
+// Random Number Generator
+// generates random numbers
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	srand(static_cast<unsigned int>(time(0))); // seed random number generator
+
+	int randomNumber = rand(); // generate random number
+
+	int die = (randomNumber % 6) + 1; // get a number between 1 - 6
+	cout << "You rolled a " << die << endl;
+
+	return 0;
+}
