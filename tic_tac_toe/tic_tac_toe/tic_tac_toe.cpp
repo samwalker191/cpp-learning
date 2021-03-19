8e155ea1ff5c54382845ff6d5e975ffc0d3054d1
Samuel Walker <sewalker191@gmail.com>
2021-03-18 23:19:09 -0700

    Finish tic tac toe

--- tic_tac_toe/tic_tac_toe/tic_tac_toe.cpp
+++ tic_tac_toe/tic_tac_toe/tic_tac_toe.cpp
@@ -21,10 +21,15 @@ int askNumber(string question, int high = 8, int low = 0);
 char humanPiece();
 char opponent(char piece);
 void displayBoard(const vector< vector<char> >& board);
+char horizontalWin(const vector< vector<char> >& board);
+char verticalWin(const vector< vector<char> >& board);
+char diagonalWin(const vector< vector<char> >& board);
+bool isOver(const vector< vector<char> >& board);
 char winner(const vector< vector<char> >& board);
 bool isLegal(const vector< vector<char> >& board, int move);
-int humanMove(const vector< vector<char> >& board, char human);
-int computerMove(const vector< vector<char> >& board, char computer);
+void placeMark(vector <vector<char> >& board, int move, char player);
+int humanMove(const vector< vector<char> >& board);
+int computerMove(vector< vector<char> > board, char computer);
 void announceWinner(char winner, char computer, char human);
 
 int main() 
@@ -36,11 +41,39 @@ int main()
 	};
 
 	instructions();
+
 	char human = humanPiece();
 	char computer = opponent(human);
-	cout << human;
-	cout << computer;
+	char currentPlayer;
+
+	if (human == X)
+	{
+		currentPlayer = human;
+	}
+	else
+	{
+		currentPlayer = computer;
+	}
+
+	while (winner(board) == NO_ONE)
+	{
+		displayBoard(board);
+		if (currentPlayer == human)
+		{
+			int move = humanMove(board);
+			placeMark(board, move, human);
+			currentPlayer = opponent(human);
+		}
+		else {
+			int move = computerMove(board, computer);
+			placeMark(board, move, computer);
+			currentPlayer = opponent(computer);
+		}
+	}
 	
+	displayBoard(board);
+	announceWinner(winner(board), computer, human);
+
 	return 0;
 }
 
@@ -111,4 +144,207 @@ int askNumber(string question, int high, int low)
 	} while (number < low || number > high);
 
 	return number;
+}
+
+void displayBoard(const vector< vector<char> >& board)
+{
+	system("cls");
+	cout << "\n";
+	for (size_t i = 0; i < 3; i++)
+	{
+		cout << " " << board[i][0] << " | " << board[i][1] << " | " << board[i][2] << " \n";
+
+		if (i < 2) 
+		{
+			cout << " ---------\n";
+		}
+	}
+}
+
+char horizontalWin(const vector< vector<char> >& board)
+{
+	for (size_t i = 0; i < 3; i++)
+	{
+		if (board[i][0] == board[i][1] && board[i][0] == board[i][2])
+		{
+			if (board[i][0] != EMPTY)
+			{
+				return board[i][0];
+			}
+		}
+	}
+	return NO_ONE;
+}
+
+char verticalWin(const vector< vector<char> >& board)
+{
+	for (size_t i = 0; i < 3; i++)
+	{
+		if (board[0][i] == board[1][i] && board[0][i] == board[2][i])
+		{
+			if (board[0][i] != EMPTY)
+			{
+				return board[0][i];
+			}
+		}
+	}
+	return NO_ONE;
+}
+
+char diagonalWin(const vector< vector<char> >& board)
+{
+	if (board[0][0] == board[1][1] && board[0][0] == board[2][2])
+	{
+		if (board[0][0] != EMPTY)
+		{
+			return board[0][0];
+		}
+	}
+
+	if (board[0][2] == board[1][1] && board[0][2] == board[2][0])
+	{
+		if (board[0][2] != EMPTY)
+		{
+			return board[0][2];
+		}
+	}
+
+	return NO_ONE;
+}
+
+bool isOver(const vector< vector<char> >& board)
+{
+	for (size_t i = 0; i < 3; i++)
+	{
+		for (size_t j = 0; j < 3; j++)
+		{
+			if (board[i][j] == EMPTY)
+			{
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+char winner(const vector< vector<char> >& board)
+{
+	if (horizontalWin(board) != NO_ONE)
+	{
+		return horizontalWin(board);
+	}
+
+	if (verticalWin(board) != NO_ONE)
+	{
+		return verticalWin(board);
+	}
+
+	if (diagonalWin(board) != NO_ONE)
+	{
+		return diagonalWin(board);
+	}
+
+	if (isOver(board))
+	{
+		return TIE;
+	}
+	else
+	{
+		return NO_ONE;
+	}
+}
+
+bool isLegal(const vector< vector<char> >& board, int move)
+{
+	int x = move / 3;
+	int y = move % 3;
+	
+	if (board[x][y] != EMPTY)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void placeMark(vector <vector<char> >& board, int move, char player)
+{
+	int x = move / 3;
+	int y = move % 3;
+	board[x][y] = player;
+}
+
+int humanMove(const vector< vector<char> >& board)
+{
+	int move;
+	do
+	{
+		move = askNumber("Please enter a number between", 8, 0);
+	} while (!isLegal(board, move));
+
+	return move;
+}
+
+int computerMove(vector< vector<char> > board, char computer)
+{
+	char human = opponent(computer);
+	for (size_t i = 0; i < 9; i++)
+	{
+		if (isLegal(board, i))
+		{
+			
+			placeMark(board, i, computer);
+			if (winner(board) == computer)
+			{
+				return i;
+			}
+			placeMark(board, i, EMPTY);
+		}
+	}
+
+	for (size_t i = 0; i < 9; i++)
+	{
+		if (isLegal(board, i))
+		{
+
+			placeMark(board, i, human);
+			if (winner(board) == human)
+			{
+				return i;
+			}
+			placeMark(board, i, EMPTY);
+		}
+	}
+
+	const int BEST_MOVES[9] = { 4, 0, 2, 6, 8, 1, 3, 5, 7 };
+	for (size_t i = 0; i < 9; i++)
+	{
+		if (isLegal(board, BEST_MOVES[i]))
+		{
+			return BEST_MOVES[i];
+		}
+	}
+}
+
+void announceWinner(char winner, char computer, char human)
+{
+	if (winner == computer)
+	{
+		cout << winner << "'s won!\n";
+		cout << "As I predicted, human, I am triumphant once more -- proof\n";
+		cout << "that computers are superior to humans in all regards.\n";
+	}
+	else if (winner == human)
+	{
+		cout << winner << "'s won!\n";
+		cout << "No, no! It cannot be! Somehow you tricked me, human.\n";
+		cout << "But never again! I, the computer, so swear it!\n";
+	}
+	else
+	{
+		cout << "It's a tie.\n";
+		cout << "You were most lucky, human, and somehow managed to tie me.\n";
+		cout << "Celebrate... for this is the best you will ever achieve.\n";
+	}
 }
