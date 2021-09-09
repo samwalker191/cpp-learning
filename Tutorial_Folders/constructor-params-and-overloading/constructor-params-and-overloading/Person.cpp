00fdf07455983c572c846eceff40094305c9c391
Samuel Walker <sewalker191@gmail.com>
2021-09-09 15:28:32 -0700

    constructor initialization list

--- Tutorial_Folders/constructor-params-and-overloading/constructor-params-and-overloading/Person.cpp
+++ Tutorial_Folders/constructor-params-and-overloading/constructor-params-and-overloading/Person.cpp
@@ -2,14 +2,10 @@
 #include <iostream>
 #include "Person.h"
 
-Person::Person() {
-	name = "unnamed";
-	age = 0;
+Person::Person(): name("unnamed"), age(0) {
 }
 
-Person::Person(string name, int age) {
-	this->name = name;
-	this->age = age;
+Person::Person(string name, int age): name(name), age(age) {
 
 	Person* memory = this;
 	cout << memory->toString() << endl;
