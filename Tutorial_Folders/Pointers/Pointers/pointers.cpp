634200c63e97e615cefa2e71cdb37d3bee409e1f
Samuel Walker <sewalker191@gmail.com>
2021-09-09 15:41:00 -0700

    beginning of pointers

---
+++ Tutorial_Folders/Pointers/Pointers/pointers.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+void manipulate(double* pValue) {
+	*pValue = 10.0;
+
+	cout << "Value of double in manipulate: " << *pValue << endl;
+}
+
+int main() {
+	int nValue = 8;
+	int* pnValue = &nValue; // pointer to the memory address of nValue
+	
+	cout << "Int value: " << nValue << endl;
+	cout << "Pointer to int address: " << pnValue << endl;
+
+	cout << "Int value via pointer: " << *pnValue << endl; // dereferences it
+
+	cout << "===============================" << endl;
+
+	double dValue = 123.4;
+	cout << "1. dValue: " << dValue << endl;
+	manipulate(&dValue);
+	cout << "2. dValue: " << dValue << endl;
+
+	return 0;
+}
