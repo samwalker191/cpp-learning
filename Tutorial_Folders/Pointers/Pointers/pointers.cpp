a1da5f7fd97d8b7c619cbdbb96239ca3b63af3cf
Samuel Walker <sewalker191@gmail.com>
2021-09-09 16:13:50 -0700

    Arrays and pointers

--- Tutorial_Folders/Pointers/Pointers/pointers.cpp
+++ Tutorial_Folders/Pointers/Pointers/pointers.cpp
@@ -9,20 +9,34 @@ void manipulate(double* pValue) {
 }
 
 int main() {
-	int nValue = 8;
-	int* pnValue = &nValue; // pointer to the memory address of nValue
-	
-	cout << "Int value: " << nValue << endl;
-	cout << "Pointer to int address: " << pnValue << endl;
+	//int nValue = 8;
+	//int* pnValue = &nValue; // pointer to the memory address of nValue
+	//
+	//cout << "Int value: " << nValue << endl;
+	//cout << "Pointer to int address: " << pnValue << endl;
 
-	cout << "Int value via pointer: " << *pnValue << endl; // dereferences it
+	//cout << "Int value via pointer: " << *pnValue << endl; // dereferences it
 
-	cout << "===============================" << endl;
+	//cout << "===============================" << endl;
 
-	double dValue = 123.4;
-	cout << "1. dValue: " << dValue << endl;
-	manipulate(&dValue);
-	cout << "2. dValue: " << dValue << endl;
+	//double dValue = 123.4;
+	//cout << "1. dValue: " << dValue << endl;
+	//manipulate(&dValue);
+	//cout << "2. dValue: " << dValue << endl;
 
+	string texts[] = { "one", "two", "three" };
+	string* pTexts = texts;
+
+	for (int i = 0; i < sizeof(texts) / sizeof(string); i++) {
+		cout << pTexts[i] << " " << flush;
+	}
+
+	cout << endl;
+
+	for (int i = 0; i < sizeof(texts) / sizeof(string); i++) {
+		cout << *(pTexts + i) << " " << flush;
+	}
+
+	cout << "" << endl;
 	return 0;
 }
