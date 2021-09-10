680b626e9562c437dbaf9bab2743db1d08594843
Samuel Walker <sewalker191@gmail.com>
2021-09-09 17:05:10 -0700

    copy constructor

--- Tutorial_Folders/constructor-params-and-overloading/constructor-params-and-overloading/Person.h
+++ Tutorial_Folders/constructor-params-and-overloading/constructor-params-and-overloading/Person.h
@@ -12,6 +12,7 @@ public:
 	Person();
 	Person(string newName) { name = newName; age = 0; };
 	Person(string name, int age);
+	Person(const Person& other): name(other.name), age(other.age) { cout << "Person created by copying" << endl; }; // copy constructor
 	string toString();
 };
 
--- Tutorial_Folders/constructor-params-and-overloading/constructor-params-and-overloading/main.cpp
+++ Tutorial_Folders/constructor-params-and-overloading/constructor-params-and-overloading/main.cpp
@@ -6,6 +6,9 @@ using namespace std;
 int main() {
 	Person person("Bob", 58);
 
+	Person person2 = person;
+
 	cout << person.toString() << endl;
+	cout << person2.toString() << endl;
 	return 0;
 }
